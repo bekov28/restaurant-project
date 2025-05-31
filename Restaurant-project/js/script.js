a0401f7651c2383ce7398ba844e292ab8b11d919
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-06-01 00:01:12 +0900

    slider added

--- Restaurant-project/js/script.js
+++ Restaurant-project/js/script.js
@@ -1,261 +1,350 @@
-"use strict";
 
-window.addEventListener("DOMContentLoaded", () => {
-  //Tabs
+"use strict"
 
-  const tabs = document.querySelectorAll(".tabheader__item"),
-    tabContents = document.querySelectorAll(".tab_content"),
-    tabParents = document.querySelector(".tabheader__items");
+window.addEventListener('DOMContentLoaded', () => {
+	// Tabs
 
-  function hideTabContents() {
-    tabContents.forEach((tabContent) => {
-      tabContent.classList.add("hide");
-      tabContent.classList.remove("show");
-    });
-    tabs.forEach((tab) => {
-      tab.classList.remove("tabheader__item_active");
-    });
-  }
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabContents = document.querySelectorAll('.tab_content'),
+		tabParents = document.querySelector('.tabheader__items')
 
-  function showTabContent(index = 0) {
-    tabContents[index].classList.add("show", "fade");
-    tabContents[index].classList.remove("hide");
-    tabs[index].classList.add("tabheader__item_active");
-  }
+	function hideTabContents() {
+		tabContents.forEach(tabContent => {
+			tabContent.classList.add('hide')
+			tabContent.classList.remove('show')
+		})
 
-  hideTabContents();
-  showTabContent();
+		tabs.forEach(tab => {
+			tab.classList.remove('tabheader__item_active')
+		})
+	}
+	
+	function showTabContent(index = 0) {
+		tabContents[index].classList.add('show', 'fade')
+		tabContents[index].classList.remove('hide')
+		tabs[index].classList.add('tabheader__item_active')
+	}
 
-  tabParents.addEventListener("click", (event) => {
-    const target = event.target;
+	hideTabContents()
+	showTabContent()
 
-    if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((tab, index) => {
-        if (target === tab) {
-          hideTabContents();
-          showTabContent(index);
-        }
-      });
-    }
-  });
+	tabParents.addEventListener('click', event => {
+		const target = event.target
 
-  //Loader
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((tab, index) => {
+				if (target === tab) {
+					hideTabContents()
+					showTabContent(index)
+				}
+			})
+		}
+	})
 
-  const loadedWrapper = document.querySelector(".loader-wrapper");
+	// Loader
 
-  setTimeout(() => {
-    loadedWrapper.style.display = "none";
-  }, 500);
+	const loaderWrapper = document.querySelector('.loader-wrapper')
 
-  //Timer
-  const deadline = "2025-12-31";
+	setTimeout(() => {
+		loaderWrapper.style.display = 'none'
+	}, 1500)
 
-  function getTimeRemaining(endtime) {
-    let days, hours, minutes, seconds;
-    const time = Date.parse(endtime) - Date.parse(new Date()); //Converts the deadline string (e.g., '2025-12-31') to a timestamp (milliseconds since Jan 1, 1970).
+	// Timer
 
-    if (time <= 0) {
-      days = 0;
-      hours = 0;
-      minutes = 0;
-      seconds;
-    } else {
-      (days = Math.floor(time / (1000 * 60 * 60 * 24))),
-        (hours = Math.floor((time / (1000 * 60 * 60)) % 24)),
-        (minutes = Math.floor((time / (1000 * 60)) % 60)),
-        (seconds = Math.floor((time / 1000) % 60));
-    }
+	const deadline = '2024-02-01'
 
-    return {
-      totalTime: time,
-      days,
-      hours,
-      minutes,
-      seconds,
-    };
-  }
+	function getTimeRemaining(endtime) {
+		let days, hours, minutes, seconds
+		const time = Date.parse(endtime) - Date.parse(new Date())
 
-  function formatNumber(number) {
-    if (number >= 0 && number < 10) {
-      return `0${number}`;
-    } else {
-      return number;
-    }
-  }
+		if (time <= 0) {
+			days = 0
+			hours = 0
+			minutes = 0
+			seconds = 0
+		} else {
+			days = Math.floor(time / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+			minutes = Math.floor((time / (1000 * 60)) % 60),
+			seconds = Math.floor((time / 1000) % 60)
+		}
 
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-      days = timer.querySelector("#days"),
-      hours = timer.querySelector("#hours"),
-      minutes = timer.querySelector("#minutes"),
-      seconds = timer.querySelector("#seconds"),
-      timeInterval = setInterval(updateClock, 1000);
+		return {
+			totalTime: time,
+			days,
+			hours,
+			minutes,
+			seconds,
+		}
+	}
 
-    updateClock();
+	function formatNumber(number) {
+		if (number >= 0 && number < 10) {
+			return `0${number}`
+		} else {
+			return number
+		}
+	}
 
-    function updateClock() {
-      const time = getTimeRemaining(endtime);
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000)
 
-      days.innerHTML = formatNumber(time.days);
-      hours.innerHTML = formatNumber(time.hours);
-      minutes.innerHTML = formatNumber(time.minutes);
-      seconds.innerHTML = formatNumber(time.seconds);
+		updateClock()
 
-      if (time.totalTime <= 0) {
-        clearInterval(timeInterval);
-      }
-    }
-  }
-  setClock(".timer", deadline);
+		function updateClock() {
+			const time = getTimeRemaining(endtime)
 
-  //Modal
-  const modalOpenBtns = document.querySelectorAll("[data-modal]"),
-    modal = document.querySelector(".modal"),
-    modalCloseBtn = document.querySelector("[data-modal-close"),
-    modalDialog = document.querySelector(".modal__dialog");
+			days.textContent = formatNumber(time.days)
+			hours.textContent = formatNumber(time.hours)
+			minutes.textContent = formatNumber(time.minutes)
+			seconds.textContent = formatNumber(time.seconds)
 
-  function openModal() {
-    modal.classList.add("show", "fade");
-    modal.classList.remove("hide");
-    document.body.style.overflow = "hidden";
-    clearInterval(modalTimerId);
-  }
+			if (time.totalTime <= 0) {
+				clearInterval(timeInterval)
+			}
+		}
+	}
 
-  function closeModal() {
-    modal.classList.add("hide");
-    modal.classList.remove("show");
-    document.body.style.overflow = "";
-  }
+	setClock('.timer', deadline)
 
-  modalOpenBtns.forEach((btn) => {
-    btn.addEventListener("click", openModal);
-  });
+	// Modal
 
-  modalCloseBtn.addEventListener("click", closeModal);
+	const modalOpenBtns = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalContent = document.querySelector('.modal__content')
 
-  modal.addEventListener("click", (event) => {
-    console.log(event.target);
-    if (event.target === modal || event.target === modalDialog) {
-      closeModal();
-    }
-  });
+	function openModal() {
+		modalContent.classList.add('modal_fade')
+		modal.classList.add('show')
+		modal.classList.remove('hide')
+		document.body.style.overflow = 'hidden'
+		clearInterval(modalTimerId)
+	}
 
-  document.addEventListener("keydown", (event) => {
-    if (event.code === "Escape" && modal.classList.contains("show")) {
-      closeModal();
-    } else if (event.code === "Enter") {
-      alert("Please fill in the modal");
-    }
-  });
+	function closeModal() {
+		modal.classList.add('hide')
+		modal.classList.remove('show')
+		document.body.style.overflow = ''
+	}
 
-  const modalTimerId = setTimeout(openModal, 6000);
+	modalOpenBtns.forEach(btn => {
+		btn.addEventListener('click', openModal)
+	})
 
-  //Class
-  class OfferMenu {
-    constructor(src, alt, title, desc, discount, sale, parentSelector) {
-      this.src = src;
-      this.alt = alt;
-      this.title = title;
-      this.desc = desc;
-      this.discount = discount;
-      this.sale = sale;
-      this.parent = document.querySelector(parentSelector);
-      this.formatToUSD();
-    }
+	modal.addEventListener('click', event => {
+		if (
+			event.target === modal ||
+			event.target.getAttribute('data-modal-close') === ''
+		) {
+			closeModal()
+		}
+	})
 
-    formatToUSD() {
-      this.discount = this.discount.toLocaleString("en-US", { style: "currency", currency: "USD" });
-      this.sale = this.sale.toLocaleString("en-US", { style: "currency", currency: "USD" });
-    }
+	document.addEventListener('keydown', event => {
+		if (event.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal()
+		}
+	})
 
-    render() {
-      const element = document.createElement("div");
-      element.innerHTML = `
-              <img src="${this.src}" alt="${this.alt}" />
-              <div>
-                <h3>${this.title}</h3>
-                <p>${this.desc}</p>
-                <p><del>${this.discount}</del><span class="primary-text">${this.sale}</span></p>
-              </div>
-      `;
-      this.parent.append(element);
-    }
-  }
+	const modalTimerId = setTimeout(openModal, 50000)
 
-  const offers = [
-    {
-      src: "./img/offer1.png",
-      alt: "Quattro Pasta",
-      title: "Quattro Pasta",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-      discount: 55,
-      sale: 20,
-    },
-    {
-      src: "./img/offer2.png",
-      alt: "Quattro Pasta",
-      title: "Quattro Pasta",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-      discount: 65,
-      sale: 25,
-    },
-    {
-      src: "./img/offer3.png",
-      alt: "Quattro Pasta",
-      title: "Quattro Pasta",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-      discount: 25,
-      sale: 15,
-    },
-  ];
+	// Class
 
-  offers.forEach((offer) => {
-    const { src, alt, title, desc, discount, sale } = offer;
-    new OfferMenu(src, alt, title, desc, discount, sale, ".offers-items").render();
-  });
+	class OfferMenu {
+		constructor(src, alt, title, descr, discount, sale, parentSelector) {
+			this.src = src
+			this.alt = alt
+			this.title = title
+			this.descr = descr
+			this.discount = discount
+			this.sale = sale
+			this.parent = document.querySelector(parentSelector)
+			this.formatToUSD()
+		}
 
-  //FORM
-  const form = document.querySelector("form"),
-    telegramBot = "8194994575:AAE3EVWjjbJcyYgolIZVa3FFFP78_cgaKaU",
-    chatId = "130556670";
+		formatToUSD() {
+			this.discount = this.discount.toLocaleString('en-US', {
+				style: 'currency',
+				currency: 'USD',
+			})
+			this.sale = this.sale.toLocaleString('en-US', {
+				style: 'currency',
+				currency: 'USD',
+			})
+		}
 
-  const message = {
-    loading: "Loading...",
-    success: "Thank you for contacting us!",
-    failure: "Something went wrong",
-  };
+		render() {
+			const element = document.createElement('div')
+			element.innerHTML = `
+				<img src="${this.src}" alt="${this.alt}">
+				<div>
+					<h3>${this.title}</h3>
+					<p>${this.descr}</p>
+					<p><del>${this.discount}</del> <span class="primary-text">${this.sale}</span></p>
+				</div>
+			`
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+			this.parent.append(element)
+		}
+	}
 
-    const statusMessage = document.createElement("div");
-    statusMessage.textContent = message.loading;
-    form.append(statusMessage);
+	fetch('http://localhost:3000/offers', {
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' },
+	})
+		.then(response => response.json())
+		.then(data => {
+			data.forEach(offer => {
+				const { src, alt, descr, discount, sale, title } = offer
+				new OfferMenu(src,alt,title,descr,discount,sale,'.offers-items').render()
+			})
+		})
 
-    const formData = new FormData(form);
+	// FORM
 
-    const object = {};
+	const form = document.querySelector('form'),
+		telegramTokenBot = '6582188202:AAHHGOGsOMqUs3ELHhK35it1ykoWYwIo4co',
+		chatId = '5796596917'
 
-    formData.forEach((value, key) => {
-      object[key] = value;
-    });
+	const message = {
+		loading: 'Loading...',
+		success: 'Thanks for contacting with us',
+		failure: 'Something went wrong',
+	}
 
-    fetch(`https://api.telegram.org/bot${telegramBot}/sendMessage`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: `Name: ${object.name}, Phone: ${object.phone}`,
-      }),
-    })
-      .then(() => (statusMessage.textContent = message.success))
-      .catch(() => (statusMessage.textContent = message.failure))
-      .finally(() => {
-        setTimeout(() => {
-          statusMessage.remove();
-          form.reset();
-          closeModal();
-        }, 2000);
-      });
-  });
-});
+	form.addEventListener('submit', event => {
+		event.preventDefault()
+
+		const loader = document.createElement('div')
+		loader.classList.add('loader')
+		loader.style.width = '20px'
+		loader.style.height = '20px'
+		loader.style.marginTop = '20px'
+		form.append(loader)
+
+		const formData = new FormData(form)
+
+		const object = {}
+		formData.forEach((value, key) => {
+			object[key] = value
+		})
+
+		fetch(`https://api.telegram.org/bot${telegramTokenBot}/sendMessage`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				chat_id: chatId,
+				text: `Name: ${object.name}. Phone: ${object.phone}`,
+			}),
+		})
+			.then(() => {
+				showStatusMessage(message.success)
+				form.reset()
+			})
+			.catch(() => showStatusMessage(message.failure))
+			.finally(() => loader.remove())
+	})
+
+	function showStatusMessage(message) {
+		const modalDialog = document.querySelector('.modal__dialog')
+
+		modalDialog.classList.add('hide')
+		openModal()
+
+		const statusModal = document.createElement('div')
+		statusModal.classList.add('modal__dialog')
+		statusModal.innerHTML = `
+			<div class="modal__content">
+				<div data-modal-close class="modal__close">&times;</div>
+				<div class="modal__title">${message}</div>
+			</div>
+		`
+
+		document.querySelector('.modal').append(statusModal)
+
+		setTimeout(() => {
+			statusModal.remove()
+			modalDialog.classList.remove('hide')
+			closeModal()
+		}, 4000)
+	}
+
+	// SLIDER
+
+	const slides = document.querySelectorAll(".offer__slide"),
+		prev = document.querySelector(".offer__slider-prev"),
+		next = document.querySelector(".offer__slider-next"),
+		total = document.querySelector("#total"),
+		current = document.querySelector("#current"),
+		slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+		slidesInner = document.querySelector(".offer__slider-inner"),
+		width = window.getComputedStyle(slidesWrapper).width
+
+	let slideIndex = 1,
+		offset = 0
+
+	if(slides.length < 10) {
+		total.textContent = `0${slides.length}`
+		current.textContent = `0${slideIndex}`
+	}else {
+		total.textContent = slides.length
+		current.textContent = slideIndex
+	}
+
+	slidesInner.style.width = 100 * slides.length + "%"
+	slidesInner.style.display = "flex"
+	slidesInner.style.transition = "all .5s ease"
+
+	slidesWrapper.style.overflow = "hidden"
+
+	slides.forEach(slide => {
+		slide.style.width = width
+	})
+
+	next.addEventListener("click", () => {
+		if(offset === +width.slice(0, width.length - 2) * (slides.length - 1)) { 
+			offset = 0
+		} else {
+			offset += +width.slice(0, width.length - 2)
+		}
+		slidesInner.style.transform = `translateX(-${offset}px)`
+
+		if(slideIndex === slides.length) {
+			slideIndex = 1
+		}else {
+			slideIndex++
+		}
+
+		if(slides.length < 10) {
+			current.textContent = `0${slideIndex}`
+		} else {
+			current.textContent = slideIndex
+		}
+	})
+
+	prev.addEventListener("click", () => {
+		if(offset === 0) { 
+			offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+		} else {
+			offset -= +width.slice(0, width.length - 2)
+		}
+		slidesInner.style.transform = `translateX(-${offset}px)`
+
+		if(slideIndex === 1) {
+			slideIndex = slides.length
+		}else {
+			slideIndex--
+		}
+
+		if(slides.length < 10) {
+			current.textContent = `0${slideIndex}`
+		} else {
+			current.textContent = slideIndex
+		}
+	})
+})
